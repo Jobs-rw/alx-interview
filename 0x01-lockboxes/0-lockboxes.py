4f06604518be698900c451533da56c9e58d3abee
jobs-rw <mjabush@yahoo.fr>
2024-01-11 21:25:55 +0000

    final updates

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 
-"""
-      You have n number of locked boxes in front of you.
-         Each box is numbered sequentially from 0 to n - 1
-    
-"""
-
-
 def canUnlockAll(boxes):
     """
-    Function that checks with boolean value if the list type and
-    length to invoke two for iterations one to traverse the list
+    Function that checks with a boolean value if the list type and
+    length to invoke two for iterations, one to traverse the list
     """
-    if type(boxes) is not list:
+    if not isinstance(boxes, list):
         return False
-    elif (len(boxes)) == 0:
+    elif not boxes:
         return False
-    for a in range(1, len(boxes) - 1):
+
+    for key in range(len(boxes)):
         boxes_checked = False
         for idx in range(len(boxes)):
-            boxes_checked = a in boxes[idx] and a != idx
+            boxes_checked = key in boxes[idx] and key != idx
             if boxes_checked:
                 break
-        if boxes_checked is False:
+        if not boxes_checked:
             return boxes_checked
+
     return True
