9151b8d382d967b7c87ce6b48663350aeb2586d5
jobs-rw <mjabush@yahoo.fr>
2024-01-25 15:21:51 +0000

    commit for third updates on 0-stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,39 @@
-#!/bin/bash
+#!/usr/bin/python3
+"""
+Log parsing
+"""
 
-total_size=0
-declare -A status_codes
+import sys
 
-print_metrics() {
-    echo "Total file size: $total_size"
-    for code in $(echo "${!status_codes[@]}" | tr ' ' '\n' | sort -n); do
-        echo "$code: ${status_codes[$code]}"
-    done
-}
+if __name__ == '__main__':
 
-process_line() {
-    line=$1
-    parts=($line)
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-    if [ "${#parts[@]}" -lt 9 ]; then
-        return
-    fi
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
-    ip_address=${parts[0]}
-    date=${parts[3]:1}
-    status_code=${parts[${#parts[@]}-2]}
-    file_size=${parts[${#parts[@]}-1]}
-
-    # Update total file size
-    total_size=$((total_size + file_size))
-
-    # Update status code count
-    if [[ $status_code =~ ^[0-9]+$ ]]; then
-        ((status_codes[$status_code]++))
-    fi
-}
-
-lines_processed=0
-
-trap 'print_metrics' SIGINT
-
-while read -r line; do
-    process_line "$line"
-    lines_processed=$((lines_processed + 1))
-
-    if [ $((lines_processed % 10)) -eq 0 ]; then
-        print_metrics
-    fi
-done
-
-print_metrics
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
