997f27925c10ac315b728837730fcec7f580d171
jobs-rw <mjabush@yahoo.fr>
2024-02-29 16:18:23 +0000

    second updates

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,6 +7,14 @@ Change comes from within
 def makeChange(coins, total):
     """
     Returns the fewest number of coins needed to meet a given amount total.
+    @coins: is a list of the values of the coins
+            in the possession
+    @total: given amount
+    Return:
+            fewest number of coins needed to meet total
+            *** If total is 0 or less, return 0
+            *** If total cannot be met by any number
+                of coins you have, return -1
     """
     if (type(total) is not int or type(coins) is not list):
         return -1
